6134ceaf3f76f7a8dd22dcd56761a9d4e84b62ff
Alan Couch <alan@couch.net.au>
2016-08-31 10:46:17 +1000

    Changes following reviewer comments
    Multiple replicates of random sets (200) snps. Changed naming convention of hybrid classes. Revised assignments.

--- hybridsPCA.R
+++ hybridsPCA.R
@@ -1,11 +1,10 @@
 ###################################################
 ##Author: Alan Couch
 ##Institute for Applied Ecology, University of Canberra ACT
-##Last Updated: 8 June 2016
+##Last Updated: 31 August 2016
 ##Feel free to reuse.
 
-#This script is to produce a PCA plot using snp data and meta data from a genlight object. 
-#More detail on the data and product can be seen at: #https://github.com/dnatheist/Maccullochella-hybridisation
+#This script is to produce a PCA plot using snp data and meta data from a genlight object. #More detail on the data and product can be seen at: #https://github.com/dnatheist/Maccullochella-hybridisation
 ###################################################
 #PCA plot
 require(adegenet)
@@ -21,12 +20,15 @@ ggplot(gg, aes(x=PC1, y=PC2))+
         theme_classic()+
         geom_hline(yintercept=0,linetype=2) +
         geom_vline(xintercept=0,linetype=2) +    
-        annotate("text", x = 0, y = -0.3, size=10, label = "Murray Cod") +
-        annotate("text", x = 5, y = 1.4, size=8, label = "♀MC")+
-        annotate("text", x = 21, y = 0.2, size=8, label = "♀MC")+
-        annotate("text", x = 75, y = -1, size=10, label = "Trout Cod") +
-        annotate("text", x = 10, y = 4, size=10, label = "F1xMC Backcross") +
-        annotate("text", x = 25, y = 0.9, size=10, label = "F1 Hybrid") +
+        annotate("text", x = 7, y = -1, size=10, label = "Murray Cod") +
+        annotate("text", x = 72, y = -1, size=10, label = "Trout Cod") +
+        annotate("text", x = 4, y = 0.9, size=10, label = "F1BXxMC")+
+        annotate("text", x = 18, y = 4, size=10, label = "F1BX") +
+        annotate("text", x = 36, y = 2, size=10, label = "F1 Hybrid") +
+        annotate("text", x = 5, y = 1.4, size=6, label = "♀MC",color="dark grey")+
+        annotate("text", x = 21, y = 0.2, size=6, label = "♀MC",color="dark grey")+
         xlim(-5,80)+
         xlab("Principle Component 1")+
         ylab("Principle Component 2")
+        
+
